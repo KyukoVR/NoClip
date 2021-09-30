dc7a1f1853d9e3356f39e2adea420e055e677cb0
Kokuchi <90070947+Kokuchi@users.noreply.github.com>
2021-09-30 19:06:40 -0400

    Update Class1.cs

--- Class1.cs
+++ Class1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using HarmonyLib;
 using BepInEx;
@@ -27,24 +27,38 @@ namespace NoClip
     [HarmonyPatch("Update", MethodType.Normal)]
     public class Class1
     {
-        static bool flying = false;
-        static bool notclipping = false;
-        static float speed = 12f;
         static bool primaryDown = false;
         static bool secondaryDown = false;
+        static bool kissingmonkesisveryrude = false;
+        static bool kissingmonkesiffun = true;
         private static void Postfix(GorillaLocomotion.Player __instance)
         {
             if (!PhotonNetwork.CurrentRoom.IsVisible || !PhotonNetwork.InRoom)
             {
                 List<InputDevice> list = new List<InputDevice>();
                 InputDevices.GetDevicesWithCharacteristics(UnityEngine.XR.InputDeviceCharacteristics.HeldInHand | UnityEngine.XR.InputDeviceCharacteristics.Right | UnityEngine.XR.InputDeviceCharacteristics.Controller, list);
-                list[0].TryGetFeatureValue(CommonUsages.secondaryButton, out secondaryDown);
                 list[0].TryGetFeatureValue(CommonUsages.primaryButton, out primaryDown);
+                list[0].TryGetFeatureValue(CommonUsages.secondaryButton, out secondaryDown);
 
-                if (secondaryDown)
+                if (primaryDown)
                 {
-                    foreach (MeshCollider mc in Resources.FindObjectsOfTypeAll<MeshCollider>())
-                        mc.transform.position = new Vector3(-0f, -0f, -0f);
+                    if (kissingmonkesisveryrude)
+                    {
+                        foreach (MeshCollider mc in Resources.FindObjectsOfTypeAll<MeshCollider>())
+                            mc.transform.localScale = mc.transform.localScale / 10000;
+                        kissingmonkesisveryrude = true;
+                        kissingmonkesiffun = false;
+                    }
+                    else
+                    {
+                        if (kissingmonkesiffun)
+                        {
+                            {
+                                kissingmonkesiffun = true;
+                                kissingmonkesisveryrude = false;
+                            }
+                        }
+                    }
                 }
             }
         }
